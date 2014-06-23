1964f3419e507db8a5b6ce23f23aaeef94e82f7e
Luke McComb <lucasmccomb@gmail.com>
2014-06-22 18:57:51 -0600

    Length, banana index, new array with raspberries, length of each fruit, new arry uppercase

--- fruits.rb
+++ fruits.rb
@@ -1,1 +1,22 @@
 fruits = ['apples', 'oranges', 'bananas', 'pomegranates', 'grapes']
+
+p fruits.length
+
+p fruits[2]
+
+rasp_arry = fruits.dup
+
+rasp_arry = rasp_arry.insert(1, "raspberries")
+
+p rasp_arry
+
+p fruits
+
+fruits.each do |elem|
+  puts elem.length
+end
+
+upcase_arry = fruits.dup
+
+p upcase_arry.each { |elem| elem.upcase! }
+
